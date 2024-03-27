be25b994d29b2abeb3064b4efe97c0817eed0a88
Barry Guda <barry.guda@pictureworks.com.au>
2024-03-27 07:32:30 +0100

    add db for persistence storage

---
+++ Config.py
@@ -0,0 +1,7 @@
+class Config:
+
+    DEBUG = True
+
+    SQLALCHEMY_DATABASE_URI = 'postgresql+psycopg2://smilecookadm:mysecretpassword@localhost/smilecook'
+
+    SQLALCHEMY_TRACK_MODIFICATIONS = False
